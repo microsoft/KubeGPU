6290ae1375ea81f6a2097779d000c519af4cbe74
Sanjeev Mehrotra <sanjeevm0@hotmail.com>
2017-09-21 16:54:51 -0700

    build skeleton CRI

--- cri/dockergpu/dockergpu_container.go
+++ cri/dockergpu/dockergpu_container.go
@@ -1,11 +1,9 @@
 package dockergpucri
 
-// import (
-// 	"k8s.io/kubernetes/pkg/kubelet/apis/cri";
-// 	"k8s.io/kubernetes/pkg/kubelet"
-// )
-
 import (
+	"time"
+
+	runtimeapi "k8s.io/kubernetes/pkg/kubelet/apis/cri/v1alpha1/runtime"
 	"k8s.io/kubernetes/pkg/kubelet/dockershim"
 )
 
@@ -13,34 +11,82 @@ type dockerGPUService struct {
 	dockerService dockershim.DockerService
 }
 
-func (dockerGPUService *d) Version(apiVersion string) (*runtimeapi.VersionResponse, error) {
+func (d *dockerGPUService) Version(apiVersion string) (*runtimeapi.VersionResponse, error) {
 	return d.dockerService.Version(apiVersion)
 }
 
-func (dockerGPUService *d) CreateContainer(podSandboxID string, config *runtimeapi.ContainerConfig, sandboxConfig *runtimeapi.PodSandboxConfig) (string, error) {
+func (d *dockerGPUService) CreateContainer(podSandboxID string, config *runtimeapi.ContainerConfig, sandboxConfig *runtimeapi.PodSandboxConfig) (string, error) {
 	return d.dockerService.CreateContainer(podSandboxID, config, sandboxConfig)
 }
 
-func (dockerGPUService *d) StartContainer(containerID string) error {
+func (d *dockerGPUService) StartContainer(containerID string) error {
 	return d.dockerService.StartContainer(containerID)
 }
 
-func (dockerGPUService *d) StopContainer(containerID string, timeout int64) error {
-	return 
-}
-	// RemoveContainer removes the container.
-	RemoveContainer(containerID string) error
-	// ListContainers lists all containers by filters.
-	ListContainers(filter *runtimeapi.ContainerFilter) ([]*runtimeapi.Container, error)
-	// ContainerStatus returns the status of the container.
-	ContainerStatus(containerID string) (*runtimeapi.ContainerStatus, error)
-	// ExecSync executes a command in the container, and returns the stdout output.
-	// If command exits with a non-zero exit code, an error is returned.
-	ExecSync(containerID string, cmd []string, timeout time.Duration) (stdout []byte, stderr []byte, err error)
-	// Exec prepares a streaming endpoint to execute a command in the container, and returns the address.
-	Exec(*runtimeapi.ExecRequest) (*runtimeapi.ExecResponse, error)
-	// Attach prepares a streaming endpoint to attach to a running container, and returns the address.
-	Attach(req *runtimeapi.AttachRequest) (*runtimeapi.AttachResponse, error)
+func (d *dockerGPUService) StopContainer(containerID string, timeout int64) error {
+	return d.dockerService.StopContainer(containerID, timeout)
 }
 
-func (dockerGPUService *d) Create
+func (d *dockerGPUService) RemoveContainer(containerID string) error {
+	return d.dockerService.RemoveContainer(containerID)
+}
+
+func (d *dockerGPUService) ListContainers(filter *runtimeapi.ContainerFilter) ([]*runtimeapi.Container, error) {
+	return d.dockerService.ListContainers(filter)
+}
+
+func (d *dockerGPUService) ContainerStatus(containerID string) (*runtimeapi.ContainerStatus, error) {
+	return d.dockerService.ContainerStatus(containerID)
+}
+
+func (d *dockerGPUService) ExecSync(containerID string, cmd []string, timeout time.Duration) (stdout []byte, stderr []byte, err error) {
+	return d.dockerService.ExecSync(containerID, cmd, timeout)
+}
+
+func (d *dockerGPUService) Exec(request *runtimeapi.ExecRequest) (*runtimeapi.ExecResponse, error) {
+	return d.dockerService.Exec(request)
+}
+
+func (d *dockerGPUService) Attach(req *runtimeapi.AttachRequest) (*runtimeapi.AttachResponse, error) {
+	return d.dockerService.Attach(req)
+}
+
+func (d *dockerGPUService) RunPodSandbox(config *runtimeapi.PodSandboxConfig) (string, error) {
+	return d.dockerService.RunPodSandbox(config)
+}
+
+func (d *dockerGPUService) StopPodSandbox(podSandboxID string) error {
+	return d.dockerService.StopPodSandbox(podSandboxID)
+}
+
+func (d *dockerGPUService) RemovePodSandbox(podSandboxID string) error {
+	return d.dockerService.RemovePodSandbox(podSandboxID)
+}
+
+func (d *dockerGPUService) PodSandboxStatus(podSandboxID string) (*runtimeapi.PodSandboxStatus, error) {
+	return d.dockerService.PodSandboxStatus(podSandboxID)
+}
+
+func (d *dockerGPUService) ListPodSandbox(filter *runtimeapi.PodSandboxFilter) ([]*runtimeapi.PodSandbox, error) {
+	return d.dockerService.ListPodSandbox(filter)
+}
+
+func (d *dockerGPUService) PortForward(req *runtimeapi.PortForwardRequest) (*runtimeapi.PortForwardResponse, error) {
+	return d.dockerService.PortForward(req)
+}
+
+func (d *dockerGPUService) ContainerStats(req *runtimeapi.ContainerStatsRequest) (*runtimeapi.ContainerStatsResponse, error) {
+	return d.dockerService.ContainerStats(req)
+}
+
+func (d *dockerGPUService) ListContainerStats(req *runtimeapi.ListContainerStatsRequest) (*runtimeapi.ListContainerStatsResponse, error) {
+	return d.dockerService.ListContainerStats(req)
+}
+
+func (d *dockerGPUService) UpdateRuntimeConfig(runtimeConfig *runtimeapi.RuntimeConfig) error {
+	return d.dockerService.UpdateRuntimeConfig(runtimeConfig)
+}
+
+func (d *dockerGPUService) Status() (*runtimeapi.RuntimeStatus, error) {
+	return d.dockerService.Status()
+}
