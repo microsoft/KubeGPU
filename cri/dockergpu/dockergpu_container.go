23567cf1cdc545bd84e5821367d114e1443ca78e
Sanjeev Mehrotra <sanjeevm0@hotmail.com>
2017-10-31 11:56:25 -0700

    mass move to upper vendor

--- cri/dockergpu/dockergpu_container.go
+++ cri/dockergpu/dockergpu_container.go
@@ -1,19 +1,18 @@
 package main
 
 import (
-	goflag "flag"
 	"fmt"
 	"net"
 	"net/http"
 	"net/url"
 	"os"
 	"strconv"
-	"strings"
 	"time"
 
 	"github.com/golang/glog"
 	"github.com/spf13/pflag"
 
+	"k8s.io/apiserver/pkg/util/flag"
 	"k8s.io/apiserver/pkg/util/logs"
 	"k8s.io/kubernetes/cmd/kubelet/app"
 	"k8s.io/kubernetes/cmd/kubelet/app/options"
@@ -257,36 +256,6 @@ func DockerGPUInit(c *kubeletconfiginternal.KubeletConfiguration, r *options.Con
 // 		return nil, err
 // 	}
 // }
-
-// WordSepNormalizeFunc changes all flags that contain "_" separators
-func WordSepNormalizeFunc(f *pflag.FlagSet, name string) pflag.NormalizedName {
-	if strings.Contains(name, "_") {
-		return pflag.NormalizedName(strings.Replace(name, "_", "-", -1))
-	}
-	return pflag.NormalizedName(name)
-}
-
-// WarnWordSepNormalizeFunc changes and warns for flags that contain "_" separators
-func WarnWordSepNormalizeFunc(f *pflag.FlagSet, name string) pflag.NormalizedName {
-	if strings.Contains(name, "_") {
-		nname := strings.Replace(name, "_", "-", -1)
-		glog.Warningf("%s is DEPRECATED and will be removed in a future version. Use %s instead.", name, nname)
-
-		return pflag.NormalizedName(nname)
-	}
-	return pflag.NormalizedName(name)
-}
-
-// InitFlags normalizes, parses, then logs the command line flags
-func InitFlags() {
-	pflag.CommandLine.SetNormalizeFunc(WordSepNormalizeFunc)
-	pflag.CommandLine.AddGoFlagSet(goflag.CommandLine)
-	pflag.Parse()
-	pflag.VisitAll(func(flag *pflag.Flag) {
-		glog.V(4).Infof("FLAG: --%s=%q", flag.Name, flag.Value)
-	})
-}
-
 // ====================
 // Main
 func die(err error) {
@@ -307,7 +276,7 @@ func main() {
 	options.AddKubeletConfigFlags(pflag.CommandLine, defaultConfig)
 
 	// parse the command line flags into the respective objects
-	InitFlags()
+	flag.InitFlags()
 
 	// initialize logging and defer flush
 	logs.InitLogs()
