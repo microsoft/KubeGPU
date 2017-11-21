f5289f60dec49f19ea73b157dc88c781ee40af38
Sanjeev Mehrotra <sanjeevm0@hotmail.com>
2017-11-20 16:18:52 -0800

    Fix vendor move script

--- hack/movevendor.py
+++ hack/movevendor.py
@@ -3,6 +3,7 @@ import os
 import argparse
 import textwrap
 import re
+import shutil
 
 #def splitPaths(pathToSplit):
 #    return pathToSplit.split('/')
@@ -14,13 +15,34 @@ def removeRoot(rootdir, name):
 def copyOrLink(rootdir, src, fd, dst, skippattern, isdir):
     srcName = os.path.join(src, fd)
     #srcPaths = splitPaths(srcName)
-    #exit()
     #dstName = os.path.join(dst, *(srcPaths[2:]))
     dstName = os.path.join(dst, removeRoot(rootdir, srcName))
     if re.match(skippattern, srcName) is None:
+        if os.path.islink(dstName):
+            print "Remove link: {0}".format(dstName)
+            os.unlink(dstName)
+        if not isdir:
+            if os.path.exists(dstName):
+                print "Remove existing file: {0}".format(dstName)
+                os.remove(dstName)
+        parentDir = os.path.dirname(dstName)
+        if not os.path.exists(parentDir):
+            print "Make parent directory: {0}".format(os.path.dirname(dstName))
+            os.mkdir(os.path.dirname(dstName))
         if os.path.islink(srcName):
             dstLinkTo = os.path.normpath(os.path.join(src, os.readlink(srcName)))
-            print "S: {0} N:{1} L:{2} DN:{3} D:{4}".format(src, srcName, os.readlink(srcName), dstName, dstLinkTo)
+            dstLinkConvt = os.path.relpath(dstLinkTo, os.path.dirname(dstName))
+            print "S:{0} N:{1} L:{2}".format(src, srcName, os.readlink(srcName))
+            print "D:{0} DL:{1} DC:{2}".format(dstName, dstLinkTo, dstLinkConvt)
+            print "Symlink Src: {0} Dst: {1}".format(dstLinkConvt, dstName)
+            os.symlink(dstLinkConvt, dstName)
+        elif isdir:
+            if not os.path.exists(dstName):
+                print "Create directory: {0}".format(dstName)
+                os.mkdir(dstName)
+        else:
+            print "Copy file Src: {0} Dst: {1}".format(srcName, dstName)
+            shutil.copy(srcName, dstName)
 
 def recurse(dir, dst, skippattern):
     for root, folders, files in os.walk(dir, topdown=True):
