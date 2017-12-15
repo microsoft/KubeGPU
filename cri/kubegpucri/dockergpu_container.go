ddd1952f2ce9570aa4079597f31ce10dd06623f6
Sanjeev Mehrotra <sanjeevm0@hotmail.com>
2017-12-15 13:33:00 -0800

    fix pod retrieval issues

--- cri/kubegpucri/dockergpu_container.go
+++ cri/kubegpucri/dockergpu_container.go
@@ -8,9 +8,11 @@ import (
 	"strconv"
 	"time"
 
+	"github.com/KubeGPU/cri/kubeadvertise"
 	"github.com/golang/glog"
 	"github.com/spf13/pflag"
 
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 	utilnet "k8s.io/apimachinery/pkg/util/net"
 	"k8s.io/apiserver/pkg/util/flag"
 	"k8s.io/apiserver/pkg/util/logs"
@@ -31,14 +33,22 @@ import (
 // implementation of runtime service -- have to implement entire docker service
 type dockerGPUService struct {
 	dockershim.DockerService
+	advertiser *kubeadvertise.DeviceAdvertiser
 }
 
 // DockerService => RuntimeService => ContainerManager
 func (d *dockerGPUService) CreateContainer(podSandboxID string, config *runtimeapi.ContainerConfig, sandboxConfig *runtimeapi.PodSandboxConfig) (string, error) {
 	// overwrite config.Devices here & then call CreateContainer ...
 	podName := config.Labels[kubelettypes.KubernetesPodNameLabel]
+	podNameSpace := config.Labels[kubelettypes.KubernetesPodNamespaceLabel]
 	containerName := config.Labels[kubelettypes.KubernetesContainerNameLabel]
 	glog.V(3).Infof("Creating container for pod %v container %v", podName, containerName)
+	opts := metav1.GetOptions{}
+	pod, err := d.advertiser.KubeClient.Core().Pods(podNameSpace).Get(podName, opts)
+	if err != nil {
+		glog.Errorf("Retrieving pod %v gives error %v", podName, err)
+	}
+	glog.V(3).Infof("Pod Spec: %v", pod.Spec)
 	return d.DockerService.CreateContainer(podSandboxID, config, sandboxConfig)
 }
 
@@ -55,30 +65,30 @@ func (d *dockerGPUService) Exec(request *runtimeapi.ExecRequest) (*runtimeapi.Ex
 
 // =====================
 // Start the shim
-func GetHostName(f *options.KubeletFlags) (string, error) {
+func GetHostName(f *options.KubeletFlags) (string, string, error) {
 	// 1) Use nodeIP if set
 	// 2) If the user has specified an IP to HostnameOverride, use it
 	// 3) Lookup the IP from node name by DNS and use the first non-loopback ipv4 address
 	// 4) Try to get the IP from the network interface used as default gateway
 	name := ""
+	hostName := nodeutil.GetHostname(f.HostnameOverride)
 	if f.NodeIP != "" {
 		name = f.NodeIP
 	} else {
-		hostName := nodeutil.GetHostname(f.HostnameOverride)
 		var addr net.IP
 		if addr = net.ParseIP(hostName); addr == nil {
 			var err error
 			addr, err = utilnet.ChooseHostInterface()
 			if err != nil {
-				return "", err
+				return "", hostName, err
 			}
 		}
 		name = addr.String()
 	}
-	return name, nil
+	return name, hostName, nil
 }
 
-func DockerGPUInit(f *options.KubeletFlags, c *componentconfig.KubeletConfiguration, r *options.ContainerRuntimeOptions) error {
+func DockerGPUInit(s *options.KubeletServer, f *options.KubeletFlags, c *componentconfig.KubeletConfiguration, r *options.ContainerRuntimeOptions) error {
 	// Create docker client.
 	dockerClient := libdocker.ConnectToDockerOrDie(r.DockerEndpoint, c.RuntimeRequestTimeout.Duration,
 		r.ImagePullProgressDeadline.Duration)
@@ -106,8 +116,8 @@ func DockerGPUInit(f *options.KubeletFlags, c *componentconfig.KubeletConfigurat
 	}
 
 	// Initialize streaming configuration. (Not using TLS now)
-	hostName, err := GetHostName(f)
-	glog.V(2).Infof("Using hostname %v", hostName)
+	hostName, nodeName, err := GetHostName(f)
+	glog.V(2).Infof("Using hostname %v nodeName %v", hostName, nodeName)
 	if err != nil {
 		return err
 	}
@@ -138,7 +148,11 @@ func DockerGPUInit(f *options.KubeletFlags, c *componentconfig.KubeletConfigurat
 		return err
 	}
 
-	dsGPU := &dockerGPUService{DockerService: ds}
+	da, err := kubeadvertise.NewDeviceAdvertiser(s, nodeName)
+	if err != nil {
+		return err
+	}
+	dsGPU := &dockerGPUService{DockerService: ds, advertiser: da}
 
 	glog.V(2).Infof("Starting the GRPC server for the docker CRI shim.")
 	server := dockerremote.NewDockerServer(c.RemoteRuntimeEndpoint, dsGPU)
@@ -185,7 +199,7 @@ func main() {
 	verflag.PrintAndExitIfRequested()
 
 	// run the gpushim
-	if err := DockerGPUInit(&s.KubeletFlags, &s.KubeletConfiguration, &s.ContainerRuntimeOptions); err != nil {
+	if err := DockerGPUInit(s, &s.KubeletFlags, &s.KubeletConfiguration, &s.ContainerRuntimeOptions); err != nil {
 		fmt.Fprintf(os.Stderr, "error: %v\n", err)
 		os.Exit(1)
 	}
